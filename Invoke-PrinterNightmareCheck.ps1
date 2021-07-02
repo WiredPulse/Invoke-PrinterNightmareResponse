49b3acc401b7353a69b21f609fa59193bb415d75
Fernando Tomlinson <WiredPulse@users.noreply.github.com>
2021-07-01 20:41:32 -0400

    Update Invoke-PrinterNightmareCheck.ps1

--- Invoke-PrinterNightmareCheck.ps1
+++ Invoke-PrinterNightmareCheck.ps1
@@ -1,12 +1,13 @@
 ﻿<#
-https://github.com/afwu/PrintNightmare
+    .SYNOPSIS
+        The current two POCs will write the DLL supplied during exploitatio to 'C:\Windows\System32\spool\drivers\x64\3' and to 
+        'C:\Windows\System32\spool\drivers\x64\3\Old'. This code retrieves all unsigned DLLs in the first location checks if any are in the second 
+        location. If a DLL is in both locations, that could be indicative of possible malicious activity.
 
-Project notes that the DLL will be written to 'C:\Windows\System32\spool\drivers\x64\3' and to 
-'C:\Windows\System32\spool\drivers\x64\3\Old'. This retrieves all unsigned DLLs in the first location
-checks if any are in the second location. If a DLL is in both locations, that could be indicative of possible activity
-
-
+    .LINK
+        https://github.com/afwu/PrintNightmare
 #>
+
 $dll = Get-ChildItem C:\Windows\System32\spool\drivers\x64\3
 $sigs = Get-AuthenticodeSignature $dll.fullname -ErrorAction SilentlyContinue
 foreach($sig in $sigs){
@@ -22,6 +23,5 @@ foreach($item in $old){
         {
         write-host -ForegroundColor Cyan "[+] " -NoNewline; Write-Host -ForegroundColor red "$item possibly indicative of CVE-2021-1675 explotation activity"
     }
-
 }
 
