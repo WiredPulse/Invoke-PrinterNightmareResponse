e30e31bf1fd7e0c4b0dd843fd719550dc2aee851
Fernando Tomlinson <WiredPulse@users.noreply.github.com>
2021-07-01 20:53:27 -0400

    Update Enable-PrintLogging.ps1

--- Enable-PrintLogging.ps1
+++ Enable-PrintLogging.ps1
@@ -4,5 +4,4 @@ if((Get-LogProperties 'Microsoft-windows-printservice/operational').enabled -eq 
     Set-LogProperties -LogDetails $logdeets
 }
 
-Get-ItemProperty 'HKLM:\system\CurrentControlSet\Control\Print\Environments\Windows x64\Drivers\Version-3'
 
